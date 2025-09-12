fa7295546c1981093d28c5a41f26df1e4a47e471
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2025-09-12 21:25:30 +0100

    Update

--- src/project_entries/portfolio_two/PortfolioTwoEntryPage.tsx
+++ src/project_entries/portfolio_two/PortfolioTwoEntryPage.tsx
@@ -1,69 +1,88 @@
 import ProjWrapper from "../../components/project-entry-utils/project_wrapper/ProjWrapper.tsx"
+import P_p from "../../components/project-entry-utils/P_p.tsx"
+import P_a from "../../components/project-entry-utils/P_a.tsx"
+import { websiteProject } from "../website/website.tsx"
+import PProjLink from "../../components/project-entry-utils/PProjLink.tsx"
+import PCodeSrc from "../../components/project-entry-utils/p-code/PCodeSrc.tsx"
+import code from "./assets/code.png"
+import django_image from "./assets/django_image.txt"
+import react_image from "./assets/react_image.txt"
+import PLatex from "../../components/project-entry-utils/PLatex.tsx"
+import P_img from "../../components/project-entry-utils/P_img.tsx"
+import P_h1 from "../../components/project-entry-utils/headings/P_h1.tsx"
+import P_h2 from "../../components/project-entry-utils/headings/P_h2.tsx"
+import P_h3 from "../../components/project-entry-utils/headings/P_h3.tsx"
 
 export default function PortfolioTwoEntryPage() {
   return (
     <ProjWrapper>
-      {/*<P_p>*/}
-      {/*  This website was created to replace my{" "}*/}
-      {/*  <P_a href={"https://robertlucas.pythonanywhere.com"}>*/}
-      {/*    old portfolio website*/}
-      {/*  </P_a>*/}
-      {/*  , the ported write-up of which you can find{" "}*/}
-      {/*  <PProjLink to={websiteProject}>here</PProjLink>. It was created using*/}
-      {/*  Django, a templating framework for Python (i.e. a framework where the*/}
-      {/*  server is responsible for generating individual webpages to send to the*/}
-      {/*  client). This contrasts React's approach (with which this website is*/}
-      {/*  created with) where the website is a single bundle sent to the client*/}
-      {/*  (although some parts can be loaded dynamically if too big).*/}
-      {/*</P_p>*/}
+      <P_p>
+        This website was created to replace my{" "}
+        <P_a href={"https://robertlucas.pythonanywhere.com"}>
+          old portfolio website
+        </P_a>{" "}
+        (link may be inactive), the ported write-up of which you can find{" "}
+        <PProjLink to={websiteProject}>here</PProjLink>.
+      </P_p>
 
-      {/*<P_p>*/}
-      {/*  Django's approach creates a large amount of friction with dynamic*/}
-      {/*  content as you are presented with the choice of either statically*/}
-      {/*  rendering data into a template on the server, then sending this to the*/}
-      {/*  client, or sending the client Javascript to load content dynamically.*/}
-      {/*  The first option can cause first-page-load delays as the server fetches*/}
-      {/*  data for the page, whereas the latter can be very difficult to*/}
-      {/*  implement, as it must be done over conventional web requests, with all*/}
-      {/*  the drawbacks, and hard to integrate libraries of vanilla JS.*/}
-      {/*</P_p>*/}
+      <P_p>
+        One of the first languages I learnt to code was Python, and as I was
+        learning it I stumbled across Django - a Python web framework with
+        static pages and templating. In hindsight, this was quite a bad starting
+        point for web development as more abstracted frameworks such as React.
+      </P_p>
 
-      {/*<PCodeSrc*/}
-      {/*  codeSrc={django_code}*/}
-      {/*  language={"html"}*/}
-      {/*  caption={"Old HTML template code for showing the project list"}*/}
-      {/*/>*/}
+      <P_p>
+        As there is more friction between templating code and HTML than in
+        frameworks such as React, I very often found myself copy-pasting code
+        making refactoring a nightmare, not to mention the JavaScript which had
+        to manually reference HTML elements and, of course, could not interact
+        with the templating logic. The end result of all of this was a spaghetti
+        of relationships that would break when I wanted to make small tweaks, or
+        I simply wouldn't implement some features due to the time requirement.
+      </P_p>
 
-      {/*<PCodeSrc*/}
-      {/*  codeSrc={react_code}*/}
-      {/*  language={"tsx"}*/}
-      {/*  caption={"React code for the expanded project list view"}*/}
-      {/*/>*/}
+      <PCodeSrc
+        codeSrc={django_image}
+        language={"html"}
+        caption={"Old HTML template code for inserting an image"}
+      />
 
-      {/*<P_p>*/}
-      {/*  Not only does React code save time in creating dynamic UI, it also*/}
-      {/*  encourages better design patterns as better abstractions are easier to*/}
-      {/*  make.*/}
-      {/*</P_p>*/}
+      <PCodeSrc
+        codeSrc={react_image}
+        language={"tsx"}
+        caption={"The current equivalent"}
+      />
 
-      {/*<PCodeSrc*/}
-      {/*  codeSrc={django_image}*/}
-      {/*  language={"html"}*/}
-      {/*  caption={"Old HTML template code for inserting an image"}*/}
-      {/*/>*/}
+      <P_p>
+        Other than massive improvements in code structuring, being able to
+        easily integrate code libraries has greatly improved the quality of the
+        website as in the old website, things that I didn't have time to make
+        such as proper code blocks would be replaced by easier solutions such as
+        images of code, which are harder to modify or style.
+      </P_p>
 
-      {/*<PCodeSrc*/}
-      {/*  codeSrc={react_image}*/}
-      {/*  language={"tsx"}*/}
-      {/*  caption={"The current equivalent"}*/}
-      {/*/>*/}
+      <div className={"text-center"}>
+        <PLatex
+          latex={
+            "\\int_{0}^{\\infty} e^{-x^2} \\, dx = \\frac{\\sqrt{\\pi}}{2}"
+          }
+        />
+        <P_p textAlign={"center"} muted>
+          Latex rendering example
+        </P_p>
+      </div>
 
-      {/*<P_p>*/}
-      {/*  Finally, the vast number of React-compatible libraries allow the easy*/}
-      {/*  inclusion of additional functionality (such as the addition of the above*/}
-      {/*  code blocks) whereas in Django, the extra effort required to add these*/}
-      {/*  led me to use images instead.*/}
-      {/*</P_p>*/}
+      <P_img
+        image={code}
+        alt={"Image of code from this website"}
+        caption={"Try clicking on me!"}
+      />
+
+      <P_h1>This heading will appear in the contents</P_h1>
+      <P_h2>Subheading - it's all automatic</P_h2>
+      <P_h3>SubSubheading</P_h3>
+      <P_h1>Other heading</P_h1>
     </ProjWrapper>
   )
 }
