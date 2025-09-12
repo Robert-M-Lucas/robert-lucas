11a52fe033830fd48f06087087a158b8888f3396
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2025-09-12 21:39:01 +0100

    Update

--- src/project_entries/h-mail/HMailEntryPage.tsx
+++ src/project_entries/h-mail/HMailEntryPage.tsx
@@ -1,12 +1,25 @@
 import ProjWrapper from "../../components/project-entry-utils/project_wrapper/ProjWrapper.tsx"
 import P_p from "../../components/project-entry-utils/P_p.tsx"
+import P_h1 from "../../components/project-entry-utils/headings/P_h1.tsx"
+import P_h2 from "../../components/project-entry-utils/headings/P_h2.tsx"
 
 export default function HMailEntryPage() {
   return (
     <ProjWrapper>
-      <P_p>
-        Todo
-      </P_p>
+      <P_h1>Concept</P_h1>
+      <P_p>Todo</P_p>
+      <P_h2>Weaknesses</P_h2>
+
+
+      <P_h1>Protocol</P_h1>
+      <P_h2>Inter-Server Protocol</P_h2>
+      <P_h2>Server-Client Protocol</P_h2>
+
+
+      <P_h1>Implementations</P_h1>
+      <P_h2>Server</P_h2>
+      <P_h2>Client</P_h2>
+
     </ProjWrapper>
   )
 }
