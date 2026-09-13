91659009b14fca1569622539cb9b42ee3ad06751
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2026-09-13 18:56:13 +0100

    Update

--- src/routes/index/IndexPage.tsx
+++ src/routes/index/IndexPage.tsx
@@ -259,59 +259,57 @@ export default function IndexPage() {
             <p className={"mt-2"}>
               Attained a first for Year 1 finishing in the top twenty-five
               students for the year. Modules taken:
-              <ul>
-                <li>
-                  <b>Programming 1 & 2</b>: Object-oriented, imperative, and
-                  functional programming. Individual tasks and group project
-                  management and execution using the Agile methodology. Achieved
-                  the maximum contribution score awarded by other group members
-                  in all applicable projects due to a willingness to take a
-                  leadership role and go above and beyond in time dedication to
-                  the project.
-                </li>
-                <li>
-                  <b>Artificial Intelligence</b>: Fundamentals of AI including
-                  ethical considerations, algorithms, and probability with
-                  Bayesian Networks. Created a full neural network with
-                  backpropagation and k-fold cross-validation for the final
-                  coursework.
-                </li>
-                <li>
-                  <b>Discrete Mathematics and Databases</b>: Sets, functions,
-                  relations, finite state machines, propositional and predicate
-                  calculus. Relational algebra, design, queries, injection
-                  attacks and legal considerations for databases.
-                </li>
-                <li>
-                  <b>Computational Mathematics</b>: Linear algebra, series,
-                  elemental number theory, and mathematical foundations of
-                  cryptography including public key cryptography.
-                </li>
-              </ul>
             </p>
-            <p>
-              Attained a first for Year 2. Modules taken:
-              <ul>
-                <li>
-                  <b>Software Engineering</b>: -
-                </li>
-                <li>
-                  <b>Algorithms and Complexity</b>: -
-                </li>
-                <li>
-                  <b>Machine Learning</b>: -
-                </li>
-                <li>
-                  <b>Visual Computing</b>: -
-                </li>
-                <li>
-                  <b>Human-Computer Interaction</b>: -
-                </li>
-                <li>
-                  <b>Advanced Programming</b>: -
-                </li>
-              </ul>
-            </p>
+            <ul>
+              <li>
+                <b>Programming 1 & 2</b>: Object-oriented, imperative, and
+                functional programming. Individual tasks and group project
+                management and execution using the Agile methodology. Achieved
+                the maximum contribution score awarded by other group members in
+                all applicable projects due to a willingness to take a
+                leadership role and go above and beyond in time dedication to
+                the project.
+              </li>
+              <li>
+                <b>Artificial Intelligence</b>: Fundamentals of AI including
+                ethical considerations, algorithms, and probability with
+                Bayesian Networks. Created a full neural network with
+                backpropagation and k-fold cross-validation for the final
+                coursework.
+              </li>
+              <li>
+                <b>Discrete Mathematics and Databases</b>: Sets, functions,
+                relations, finite state machines, propositional and predicate
+                calculus. Relational algebra, design, queries, injection attacks
+                and legal considerations for databases.
+              </li>
+              <li>
+                <b>Computational Mathematics</b>: Linear algebra, series,
+                elemental number theory, and mathematical foundations of
+                cryptography including public key cryptography.
+              </li>
+            </ul>
+            <p>Attained a first for Year 2. Modules taken:</p>
+            <ul>
+              <li>
+                <b>Software Engineering</b>: -
+              </li>
+              <li>
+                <b>Algorithms and Complexity</b>: -
+              </li>
+              <li>
+                <b>Machine Learning</b>: -
+              </li>
+              <li>
+                <b>Visual Computing</b>: -
+              </li>
+              <li>
+                <b>Human-Computer Interaction</b>: -
+              </li>
+              <li>
+                <b>Advanced Programming</b>: -
+              </li>
+            </ul>
             <p>
               Year 3 will include a year-long project, reinforcement learning,
               logic and semantics, computational complexity, advanced computer
@@ -328,36 +326,30 @@ export default function IndexPage() {
               time.
             </p>
             <h2>Awards</h2>
-            <p>
-              Year 13 (2022 – 2023)
-              <ul>
-                <li>
-                  National Cyber Awards – Cyber Student of the Year Finalist
-                  <br />
-                  <a
-                    href={"https://thenationalcyberawards.org/2022-finalists/"}
-                    target={"_blank"}
-                  >
-                    https://thenationalcyberawards.org/2022-finalists/
-                  </a>
-                </li>
-                <li>UKMT Senior Maths Challenge – GOLD</li>
-              </ul>
-            </p>
-            <p>
-              Year 12 (2021 – 2022)
-              <ul>
-                <li>UKMT Senior Maths Challenge – SILVER</li>
-                <li>Bebras Challenge – Distinction</li>
-                <li>Extended Project Qualification (Computer Science) – A*</li>
-              </ul>
-            </p>
-            <p>
-              Year 11 (2020 – 2021)
-              <ul>
-                <li>UKMT Intermediate Maths Challenge – Gold</li>
-              </ul>
-            </p>
+            <p>Year 13 (2022 – 2023)</p>
+            <ul>
+              <li>
+                National Cyber Awards – Cyber Student of the Year Finalist
+                <br />
+                <a
+                  href={"https://thenationalcyberawards.org/2022-finalists/"}
+                  target={"_blank"}
+                >
+                  https://thenationalcyberawards.org/2022-finalists/
+                </a>
+              </li>
+              <li>UKMT Senior Maths Challenge – GOLD</li>
+            </ul>
+            <p>Year 12 (2021 – 2022)</p>
+            <ul>
+              <li>UKMT Senior Maths Challenge – SILVER</li>
+              <li>Bebras Challenge – Distinction</li>
+              <li>Extended Project Qualification (Computer Science) – A*</li>
+            </ul>
+            <p>Year 11 (2020 – 2021)</p>
+            <ul>
+              <li>UKMT Intermediate Maths Challenge – Gold</li>
+            </ul>
           </div>
         </Container>
       </section>
