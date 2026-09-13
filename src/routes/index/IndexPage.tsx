259e9cc0ac972a26eda8167917a08853e4442290
Robert-M-Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2026-09-13 18:40:32 +0100

    Update

--- src/routes/index/IndexPage.tsx
+++ src/routes/index/IndexPage.tsx
@@ -17,7 +17,7 @@ import ScrollToTop from "../../components/ScrollToTop.tsx"
 import { allTechnologies } from "../projects/single-project-page/technology.tsx"
 import "./index-page.css"
 import { scrollStorageKey } from "../projects/ProjectsIndexPage.tsx"
-import { ArrowDown, GraphDownArrow } from "react-bootstrap-icons"
+import { ArrowDown, Globe, GraphDownArrow } from "react-bootstrap-icons"
 
 const transition: Transition = { duration: 1.3, ease: [0.25, 0.1, 0.25, 1] }
 const variants = {
@@ -206,57 +206,157 @@ export default function IndexPage() {
               })}
             </div>
           </div>
-
           <div className={"mb-4"}>
             <h1 className={"display-3"}>Experience</h1>
-            <h2>Trio Motion Technology</h2>
-
-            <p>
-              In placerat elit non mi gravida lacinia id quis nulla. Nullam
-              dolor mi, porttitor nec sapien a, facilisis mattis ex. Cras ut
-              ornare lacus, a iaculis velit. Morbi placerat nisi vitae venenatis
-              viverra. Quisque eu pulvinar ante. Donec porta arcu vel odio
-              sollicitudin, eu aliquam arcu imperdiet. Aenean hendrerit
-              fermentum ante eget ornare. Pellentesque ullamcorper nisl tortor,
-              in vehicula tellus efficitur nec. Curabitur ac ligula interdum,
-              ultricies tortor nec, sodales erat. Donec fringilla maximus nisl
-              sit amet accumsan. Morbi a nisl lobortis augue egestas imperdiet
-              in aliquam mauris. Phasellus nec augue fringilla arcu condimentum
-              bibendum id eu quam. Aliquam posuere ornare est, at vulputate
-              ipsum malesuada ut. Etiam consequat vestibulum fermentum.
+            <h2 className={"mb-0"}>
+              Trio Motion Technology{" "}
+              <a href={"https://triomotion.com/"} target={"_blank"}>
+                <Globe size={"20px"} />
+              </a>
+            </h2>
+            <small className="text-body-secondary">
+              2025 - 2026 | Year-Long Professional Placement
+            </small>
+            <p className={"mt-2"}>-</p>
+            <h2 className={"mb-0"}>
+              Private English, Maths, & Computer Science Tutoring
+            </h2>
+            <small className="text-body-secondary">
+              2023 - 2025 | For A-Level, GCSEs, and SATs
+            </small>
+            <p className={"mt-2"}>
+              Creating and teaching tailored lesson plans for SATs, GCSE, and
+              A-Level students in English, mathematics, and computer science to
+              complement in-class work, helping them improve upon their
+              weaknesses and reinforce their strengths.
+            </p>
+            <h2 className={"mb-0"}>
+              Vestcom{" "}
+              <a href={"https://vestcom.com/"} target={"_blank"}>
+                <Globe size={"20px"} />
+              </a>
+            </h2>
+            <small className="text-body-secondary">
+              June 2021 (4 weeks) | Software Development Internship
+            </small>
+            <p className={"mt-2"}>
+              Learning about the structure of a software company (including
+              marketing, project management and software development) and
+              gaining an understanding of how a complex full-stack system (iRex
+              M3) can be created and maintained through multiple meetings with
+              the founder of iRex and CGO of Vestcom. The last two weeks were
+              spent as a tester alongside the development team whilst continuing
+              to learn about the company.
             </p>
           </div>
           <div className={"mb-4"}>
             <h1 className={"display-3"}>Education</h1>
-            <h2>University of Bath</h2>
-            <p>
-              In placerat elit non mi gravida lacinia id quis nulla. Nullam
-              dolor mi, porttitor nec sapien a, facilisis mattis ex. Cras ut
-              ornare lacus, a iaculis velit. Morbi placerat nisi vitae venenatis
-              viverra. Quisque eu pulvinar ante. Donec porta arcu vel odio
-              sollicitudin, eu aliquam arcu imperdiet. Aenean hendrerit
-              fermentum ante eget ornare. Pellentesque ullamcorper nisl tortor,
-              in vehicula tellus efficitur nec. Curabitur ac ligula interdum,
-              ultricies tortor nec, sodales erat. Donec fringilla maximus nisl
-              sit amet accumsan. Morbi a nisl lobortis augue egestas imperdiet
-              in aliquam mauris. Phasellus nec augue fringilla arcu condimentum
-              bibendum id eu quam. Aliquam posuere ornare est, at vulputate
-              ipsum malesuada ut. Etiam consequat vestibulum fermentum.
+            <h2 className={"mb-0"}>Computer Science - University of Bath</h2>
+            <small className="text-body-secondary">
+              2023 - 2027 | BSc (Hons) Computer Science with Professional
+              Placement
+            </small>
+            <p className={"mt-2"}>
+              Attained a first for Year 1 finishing in the top twenty-five
+              students for the year. Modules taken:
+              <ul>
+                <li>
+                  <b>Programming 1 & 2</b>: Object-oriented, imperative, and
+                  functional programming. Individual tasks and group project
+                  management and execution using the Agile methodology. Achieved
+                  the maximum contribution score awarded by other group members
+                  in all applicable projects due to a willingness to take a
+                  leadership role and go above and beyond in time dedication to
+                  the project.
+                </li>
+                <li>
+                  <b>Artificial Intelligence</b>: Fundamentals of AI including
+                  ethical considerations, algorithms, and probability with
+                  Bayesian Networks. Created a full neural network with
+                  backpropagation and k-fold cross-validation for the final
+                  coursework.
+                </li>
+                <li>
+                  <b>Discrete Mathematics and Databases</b>: Sets, functions,
+                  relations, finite state machines, propositional and predicate
+                  calculus. Relational algebra, design, queries, injection
+                  attacks and legal considerations for databases.
+                </li>
+                <li>
+                  <b>Computational Mathematics</b>: Linear algebra, series,
+                  elemental number theory, and mathematical foundations of
+                  cryptography including public key cryptography.
+                </li>
+              </ul>
             </p>
-            <h2>Mark Rutherford Sixth Form</h2>
             <p>
-              In placerat elit non mi gravida lacinia id quis nulla. Nullam
-              dolor mi, porttitor nec sapien a, facilisis mattis ex. Cras ut
-              ornare lacus, a iaculis velit. Morbi placerat nisi vitae venenatis
-              viverra. Quisque eu pulvinar ante. Donec porta arcu vel odio
-              sollicitudin, eu aliquam arcu imperdiet. Aenean hendrerit
-              fermentum ante eget ornare. Pellentesque ullamcorper nisl tortor,
-              in vehicula tellus efficitur nec. Curabitur ac ligula interdum,
-              ultricies tortor nec, sodales erat. Donec fringilla maximus nisl
-              sit amet accumsan. Morbi a nisl lobortis augue egestas imperdiet
-              in aliquam mauris. Phasellus nec augue fringilla arcu condimentum
-              bibendum id eu quam. Aliquam posuere ornare est, at vulputate
-              ipsum malesuada ut. Etiam consequat vestibulum fermentum.
+              Attained a first for Year 2. Modules taken:
+              <ul>
+                <li>
+                  <b>Software Engineering</b>: -
+                </li>
+                <li>
+                  <b>Algorithms and Complexity</b>: -
+                </li>
+                <li>
+                  <b>Machine Learning</b>: -
+                </li>
+                <li>
+                  <b>Visual Computing</b>: -
+                </li>
+                <li>
+                  <b>Human-Computer Interaction</b>: -
+                </li>
+                <li>
+                  <b>Advanced Programming</b>: -
+                </li>
+              </ul>
+            </p>
+            <p>
+              Year 3 will include a year-long project, reinforcement learning,
+              logic and semantics, computational complexity, advanced computer
+              graphics, and advanced computer vision.
+            </p>
+            <h2 className={"mb-0"}>Mark Rutherford Sixth Form</h2>
+            <small className="text-body-secondary">
+              2021 - 2023 | A in Maths, Further Maths, Computer Science, and
+              Physics
+            </small>
+            <p className={"mt-2"}>
+              A in Mathematics, Further Mathematics, Physics, and Computer
+              Science. Founded and ran a STEM club for younger years during this
+              time.
+            </p>
+            <h2>Awards</h2>
+            <p>
+              Year 13 (2022 – 2023)
+              <ul>
+                <li>
+                  National Cyber Awards – Cyber Student of the Year Finalist
+                  <br />
+                  <a
+                    href={"https://thenationalcyberawards.org/2022-finalists/"}
+                    target={"_blank"}
+                  >
+                    https://thenationalcyberawards.org/2022-finalists/
+                  </a>
+                </li>
+                <li>UKMT Senior Maths Challenge – GOLD</li>
+              </ul>
+            </p>
+            <p>
+              Year 12 (2021 – 2022)
+              <ul>
+                <li>UKMT Senior Maths Challenge – SILVER</li>
+                <li>Bebras Challenge – Distinction</li>
+                <li>Extended Project Qualification (Computer Science) – A*</li>
+              </ul>
+            </p>
+            <p>
+              Year 11 (2020 – 2021)
+              <ul>
+                <li>UKMT Intermediate Maths Challenge – Gold</li>
+              </ul>
             </p>
           </div>
         </Container>
